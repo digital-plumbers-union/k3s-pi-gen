ecb7c0fca82e5922e3a038d2595af439aeb4f646
alex weidner <side.weidner@gmail.com>
2020-02-08 18:53:39 -0500

    Enable password-less sudo

---
+++ stage2/04-passwordless-sudo/03-run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# passwordless sudo needed for k3sup: 
+#   https://github.com/alexellis/k3sup/issues/100
+on_chroot << EOF
+  SUDO_USER="${FIRST_USER_NAME} echo "${FIRST_USER_NAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+EOF
