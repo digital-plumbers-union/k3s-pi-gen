84b39d7f1d541018fa0883b16e25c68f14133b3d
alex weidner <side.weidner@gmail.com>
2020-02-08 20:44:10 -0500

    Remove rootfs resize stuff because it seems to be handled by stage2/01

--- stage2/05-raspi-config-tweaks/04-run.sh
+++ stage2/05-raspi-config-tweaks/04-run.sh
@@ -3,10 +3,6 @@
 # tweak raspi-config based on Alex Ellis' recommendations from his blog:
 # https://blog.alexellis.io/test-drive-k3s-on-raspberry-pi/
 # - limit memory split with GPU
-# - change passwd.  i make it random because you should never login with passwd
-# as well as some things i dont remember where i read:
-# - increase partition size
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_memory_split 16
-  SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_expand_rootfs
 EOF
