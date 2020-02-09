b64da69d2628fc28cfc75e919e51c62f2446e24b
alex weidner <side.weidner@gmail.com>
2020-02-09 02:39:41 -0500

    fix static-ip script

--- stage2/07-set-static-ip/06-run.sh
+++ stage2/07-set-static-ip/06-run.sh
@@ -2,5 +2,5 @@
 
 # enables secure ssh from first boot
 on_chroot << EOF
-  echo "static ip_address=${STATIC_IP}" > /etc/dhcpcd.conf
+  echo "static ip_address=${STATIC_IP}" >> /etc/dhcpcd.conf
 EOF
